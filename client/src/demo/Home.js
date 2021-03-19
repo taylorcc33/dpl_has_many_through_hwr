dcde066d6864b57a438f53605ec6019f9d3eb4d2
Taylor Collins <taylorcc33@gmail.com>
2021-03-19 14:15:58 -0600

    axios get calls added

--- client/src/demo/Home.js
+++ client/src/demo/Home.js
@@ -1,6 +1,48 @@
+import Axios from "axios";
+import { useEffect, useState } from "react";
 import { Header } from "semantic-ui-react";
 
 const Home = () => {
+  const [appointments, setAppointments] = useState([]);
+  const [teachers, setTeachers] = useState([]);
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    getAppointments();
+    getTeachers();
+    getStudents();
+  }, []);
+
+  const getAppointments = async () => {
+    try {
+      let res = await Axios.get("/api/appointments");
+      setAppointments(res.data);
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getTeachers = async () => {
+    try {
+      let res = await Axios.get("/api/teachers");
+      setTeachers(res.data);
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getStudents = async () => {
+    try {
+      let res = await Axios.get("/api/students");
+      setStudents(res.data);
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <>
       <h1>1-on-1 Appointments</h1>
