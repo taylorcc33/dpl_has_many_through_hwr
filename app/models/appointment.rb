d0612bcb29596b519322b9fabac327db32bfad81
Taylor Collins <taylorcc33@gmail.com>
2021-03-19 12:50:10 -0600

    custom method added

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,11 @@
 class Appointment < ApplicationRecord
   belongs_to :teacher
   belongs_to :student
+
+  def self.all_data
+    appointments = Appointment.all
+    appointments.map do |appointment|
+      { id: appointment.id, date: appointment.date, teacher: appointment.teacher, student: appointment.student }
+    end
+  end
 end
