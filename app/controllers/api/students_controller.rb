fa8ae0392beab0f45fce47974957a74951fc4806
Taylor Collins <taylorcc33@gmail.com>
2021-03-19 12:42:11 -0600

    teacher and student models created

--- app/controllers/api/students_controller.rb
+++ app/controllers/api/students_controller.rb
@@ -1,2 +1,37 @@
 class Api::StudentsController < ApplicationController
+
+  def index
+    students = Student.All
+    render json: students
+  end
+
+  def show
+    render json: @student
+  end
+
+  def create
+    @student = Student.new(student_params)
+
+    if @student.save
+      render json: @student
+    else
+      render json: {error: @student.error}, status: 422
+    end
+  end
+
+  def destroy
+    @student.destroy
+    render json: @student
+  end
+
+  private
+
+    def set_student
+      @student = Student.find(params[:id])
+    end
+
+    def student_params
+      params.require(:student).permit(:first_name, :last_name)
+    end
+
 end
--- app/controllers/api/teachers_controller.rb
+++ app/controllers/api/teachers_controller.rb
@@ -1,2 +1,37 @@
 class Api::TeachersController < ApplicationController
+  before_action :set_teacher, only: [:show, :destroy]
+
+  def index
+    teachers = Teacher.All 
+    render json: teachers
+  end
+
+  def show
+    render json: @teacher
+  end
+
+  def create
+    @teacher = Teacher.new(teacher_params)
+
+    if @teacher.save
+      render json: @teacher
+    else
+      render json: {error: @teacher.errors}, status: 422
+    end
+  end
+
+  def destroy
+    @teacher.destroy
+    render json: @uteacher
+  end
+
+  private
+
+    def set_teacher
+      @teacher = Teacher.find(params[:id])
+    end
+
+    def teacher_params
+      params.require(:user).permit(:first_name, :last_name)
+    end
 end
